3f7a6f2868d9b5d868455f656df2aa6bf56150f3
TORUN\Torun <shahadat.imaginary@gmail.com>
2023-08-06 23:11:53 +0600

    Updated profile page.

--- src/views/Patrol-view.vue
+++ src/views/Patrol-view.vue
@@ -1,23 +1,16 @@
 <template>
   <v-app id="inspire">
-      <NavigationDrawer />
-      <v-main>
-        <Breadcrumbs />
-        <PatrolListForm />
-        <!-- <v-container>
-          <v-row>
-            <v-col md="7" sm="12"><PatrolDataTable /></v-col>
-            <v-col md="5" sm="12"><PatrolListForm /></v-col>
-          </v-row>
-        </v-container> -->
-        <Footerbar />
-      </v-main>
+    <NavigationDrawer />
+    <v-main>
+      <Breadcrumbs />
+      <PatrolListForm />
+      <Footerbar />
+    </v-main>
   </v-app>
 </template>
 
 <script setup>
 import NavigationDrawer from '@/components/NavigationDrawer.vue';
-// import PatrolDataTable from '@/components/PatrolDataTable.vue';
 import PatrolListForm from '@/components/advanced-form/PatrolListForm.vue';
 import Footerbar from '@/components/Footer.vue';
 </script>
